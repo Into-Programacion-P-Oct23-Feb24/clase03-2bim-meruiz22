cd124c69552ed1590babbd78bcec6d681efb9323
meruiz22 <meruiz22@utpl.edu.ec>
2023-12-20 15:37:39 -0500

    cambios

---
+++ Ejemplosc3/src/paquete05/Ejemplo001.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo001 {
+    public static void main(String[] args) {
+        int[]arreglo1 = new int[3];
+        for (int i=0;i<arreglo1.length;i++){
+            System.out.printf("%s\n",arreglo1[i]);
+        }
+    }
+}
+
