c745bc7c15829d13e2ff1738e7b775b7774c60d7
meruiz22 <meruiz22@utpl.edu.ec>
2023-12-20 15:40:28 -0500

    cambios

--- Ejemplosc3/src/paquete05/Ejemplo001.java
+++ Ejemplosc3/src/paquete05/Ejemplo001.java
@@ -10,7 +10,7 @@ package paquete05;
  */
 public class Ejemplo001 {
     public static void main(String[] args) {
-        int[]arreglo1 = new int[3];
+        int[]arreglo1 = new int[2];
         for (int i=0;i<arreglo1.length;i++){
             System.out.printf("%s\n",arreglo1[i]);
         }
